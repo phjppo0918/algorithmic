dbd81a22dc64935f124947f2f556d49d29e81ba2
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-08 23:27:49 +0900

    namespace

--- namespace3_dudle53.cpp
+++ namespace3_dudle53.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 
-using namespace std;
+
+
 
 int n;
 void set() {
+
 	n = 10;
 }
+
 namespace doodle {
 	int n;
-	void set();
+	void set() {
+		n = 20;
+	}
 
 	namespace google {
-	
-		void set();
 		int n;
+		void set() {
+			n = 30;
+		}
+
 	}
 }
 
 int main() {
+	using namespace std;
+	using namespace doodle;
+	//set();
 	::set();
 	doodle::set();
-	doodle::google::set();
+	google::set();
 
 	cout << ::n << endl;
 	cout << doodle::n << endl;
 	cout << doodle::google::n << endl;
 
 	return 0;
-}
-
-void doodle::set() {
-	n = 20;
-
-}
-
-void doodle::google::set() {
-	n = 40;
-
 }
